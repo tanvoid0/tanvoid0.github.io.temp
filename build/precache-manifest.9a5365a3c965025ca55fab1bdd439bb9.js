7e72eda75c7da80151e1d294584b92beb6c0ba8e
Tan <thaque20@gmail.com>
2021-09-23 05:41:01 +0100

    v 1.0.1 Github deploy build

---
+++ build/precache-manifest.9a5365a3c965025ca55fab1bdd439bb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c772fd374eead89d538f15dfb86af2aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2093d0d1476244d26b4",
+    "url": "/static/css/2.84b752e8.chunk.css"
+  },
+  {
+    "revision": "77c64943dee1742c1d44",
+    "url": "/static/css/main.1bf286e8.chunk.css"
+  },
+  {
+    "revision": "e2093d0d1476244d26b4",
+    "url": "/static/js/2.add507a0.chunk.js"
+  },
+  {
+    "revision": "e3bb9ad3cfb5b86e1e91414e563a0777",
+    "url": "/static/js/2.add507a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c64943dee1742c1d44",
+    "url": "/static/js/main.7be36b04.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
